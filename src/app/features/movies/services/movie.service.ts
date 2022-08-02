5b38b6156b4fc548db4509d091ae9ff73e128ede
Jonathan Estrada Betancur <jestradabe@gmail.com>
2022-08-01 20:37:46 -0500

    Se cargan imagenes dinamicamente

--- src/app/features/movies/services/movie.service.ts
+++ src/app/features/movies/services/movie.service.ts
@@ -1,16 +1,36 @@
-import { Observable } from 'rxjs';
+import { RecentMovie } from './../models/recentMovie';
+import { map, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient: HttpClient) { }
+  getRecentMovies(): Observable<RecentMovie[]> {
+    let response = this.httpClient
+      .get(
+        'https://api.themoviedb.org/3/movie/now_playing?api_key=a6db559033af943be136a2110dbd4b5f&language=es-ES'
+      )
+      .pipe(
+        map((x: any) => {
+          let movies: RecentMovie[] = [];
+          for (let movie of x.results) {
 
-  getRecentMovies(): Observable<any> {
-    let response = this.httpClient.get('https://api.themoviedb.org/3/movie/now_playing?api_key=a6db559033af943be136a2110dbd4b5f&language=es-ES');
+            let recentMovie: RecentMovie = {
+              id: movie.id,
+              description: movie.overview,
+              name: movie.title,
+              url: `http://image.tmdb.org/t/p/w1280${movie.backdrop_path}`
+            };
+
+            movies.push(recentMovie);
+          }
+          return movies;
+        })
+      );
     return response;
   }
 }
